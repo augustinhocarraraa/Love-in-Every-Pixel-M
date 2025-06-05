70d35eaeee4808eae3f9806a084daa5455082003
Victor <vitinho.vgzin@gmail.com>
2025-06-05 17:00:23 -0300

    atualização

--- Love-in-Every-Pixel-M/src/app/index.tsx
+++ Love-in-Every-Pixel-M/src/app/index.tsx
@@ -1,13 +1,29 @@
 // src/app/index.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+import { useRouter } from 'expo-router';
 
 export default function Home() {
+  const [name, setName] = useState('')
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá, Maiane!</Text>
-      <Button title='Entrar' onPress={() => alert('Você clicou no botão!')} />
+  <View style={styles.container}>
+      <Text style={styles.title}>Olá, {name}!</Text>
+      <Input
+        placeholder='Digite seu nome'
+        onChangeText={text => setName(text)}
+        />
+      <Button title='Entrar'
+      onPress={() => {
+      if (name.trim() !== ''){
+        router.push('/dashboard');
+      } else{
+        alert
+      }
+      }} />
     </View>
   );
 }
